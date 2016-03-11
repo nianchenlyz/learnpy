b84bb6a1f7510876fb49b767520831f50d6c404c
nianchenlyz <504954488@qq.com>
2016-03-11 14:06:39 +0800

    Create CompareDifferntFile.py

---
+++ CompareDifferntFile.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import difflib
+import sys
+
+try:
+	textfile1=sys.argv[1]
+	textfile2=sys.argv[2]
+except Exception,e:
+    print "Error:"+str(e)
+    print "Usage: CompareDefferentFil.py filename1 filename2"
+    sys.exit()
+
+def readfile(filename):
+	try:
+		fileHandle = open (filename,'rb')
+		text=fileHandle.read().splitlines()
+		fileHandle.close()
+		return text
+	except IOError as error:
+		print ('Read file Error:'+str(error))
+		sys.exit()
+if textfile1=="" or textfile2=="":
+	print "Usage: CompareDefferentFil.py filename1 filename2"
+	sys.exit()
+
+text1_lines=readfile(textfile1)
+text2_lines=readfile(textfile2)
+
+d=difflib.HtmlDiff()
+print d.make_file(text1_lines,text2_lines)
+
+
+#调用时候执行命令
+
+#python CompareDifferntFile.py E:\1.txt E:\2.txt >ff.html
